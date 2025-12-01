a1a69ee03a4d12579805f8ad00904dc1c13326a7
gajurelkshitiz <kshitiz.purbct042@gmail.com>
2025-12-01 14:38:09 +0545

    creating config and models folder

---
+++ config/db.go
@@ -0,0 +1,51 @@
+package database
+
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+)
+
+var DB *gorm.DB
+
+func connectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Build a dsn from env:
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	// Connect to a postgresql database:
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Failed to get sql.DB from gorm DB: %v", err)
+	}
+	if err := sqlDB.Ping(); err != nil {
+		log.Fatalf("Database ping failed: %v", err)
+	}
+
+	DB = db
+	fmt.Println("Connected to PostgreSQL successfully!")
+
+
+}
+
--- main.go
+++ main.go
@@ -1,22 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-	"context"
+	"github.com/kshitizgajurel/go_first_project/config"
+	"github.com/kshitizgajurel/go_first_project/models"
 )
 
-type Product struct {
-	gorm.Model
-	Code string
-	Price uint
-}
+// type Product struct {
+// 	gorm.Model
+// 	Code string
+// 	Price uint
+// }
 
 func main() {
+
 	// create a gin router with default middleware
 	r := gin.Default()
 
@@ -35,30 +34,12 @@ func main() {
 		})
 	})
 
-	// ** Just for practising: sqlite database connection using - gorm
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
-	if err != nil {
-		panic("failed to connect database")
-	}
+	database.connectDB()
 
-	ctx := context.Background()
+	// Auto migrate tables
+	database.DB.AutoMigrate(&models.User{})
 
-	// Migrate the schema
-	db.AutoMigrate(&Product{})
-
-	// create
-	err = gorm.G[Product](db).Create(ctx, &Product{Code: "D42", Price: 100})
-
-	// Read
-	// product, err := gorm.G[Product](db).Where("id = ?", 1).First(ctx)  // find product with integer primary key
-	product, err := gorm.G[Product](db).Where("code = ?", "D42").Find(ctx) // find product with code D42
-
-	// Update - update product's price to 200
-	err = gorm.G[Product](db).Where("id = ?", product.ID).Update(ctx, "Price", 200)
-
-
-	fmt.Println("## Product after read:", product)
 
 
 
